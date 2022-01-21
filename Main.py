cdc1212ddad3d0a8e0f38a5b4954b555cf9b72b1
danyarmarkin <danila.iarmarkin@gmail.com>
2022-01-21 15:24:50 +0300

    1

--- Main.py
+++ Main.py
@@ -1,37 +1,40 @@
 import cv2
-import numpy as np
+import datetime
 import tkinter
 
-# Create a VideoCapture object and read from input file
-cap = cv2.VideoCapture('video.mp4')
+while True:
+    moscow_time = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=3)))
+    time = int(str(moscow_time).split()[1].split(":")[0])
 
-win = tkinter.Tk()
-screen_width = win.winfo_screenwidth()
-screen_height = win.winfo_screenheight()
+    win = tkinter.Tk()
+    screen_width = win.winfo_screenwidth()
+    screen_height = win.winfo_screenheight()
 
-if not cap.isOpened():
-    print("Error opening video  file")
+    cap = cv2.VideoCapture('video1.mp4')
 
-while cap.isOpened():
+    if 9 <= time < 14:
+        cap = cv2.VideoCapture("video2.mp4")
+    if 14 <= time < 17:
+        cap = cv2.VideoCapture("video3.mp4")
+    if 17 <= time < 24:
+        cap = cv2.VideoCapture("video4.mp4")
 
-    ret, frame = cap.read()
-    if ret:
+    if not cap.isOpened():
+        print("Error opening video  file")
 
-        cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
-        cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-        sized_frame = cv2.resize(frame, (screen_width, screen_height))
-        cv2.imshow('window', sized_frame)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
 
-        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
+            cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+            sized_frame = cv2.resize(frame, (screen_width, screen_height))
+            cv2.imshow('window', sized_frame)
+
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                exit(0)
+        else:
             break
 
-    # Break the loop
-    else:
-        break
+    cap.release()
 
-# When everything done, release
-# the video capture object
-cap.release()
-
-# Closes all the frames
-cv2.destroyAllWindows()
