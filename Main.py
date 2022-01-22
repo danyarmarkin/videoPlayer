60d16b8712a87560950cabd00a110406ccf50c28
danyarmarkin <danila.iarmarkin@gmail.com>
2022-01-22 21:22:55 +0300

    1

--- Main.py
+++ Main.py
@@ -6,20 +6,30 @@ from UI import *
 def start():
     while True:
         moscow_time = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=3)))
-        current_time = int(str(moscow_time).split()[1].split(":")[0])
+        current_h = int(str(moscow_time).split()[1].split(":")[0])
+        current_m = int(str(moscow_time).split()[1].split(":")[1])
 
         win = tkinter.Tk()
         screen_width = win.winfo_screenwidth()
         screen_height = win.winfo_screenheight()
+        win.destroy()
+
+        tasks = []
+        file = open("settings.txt", "r")
+        for line in file.readlines():
+            t = line.split()
+            if len(t) != 5:
+                continue
+            task = Task(int(t[0]), int(t[1]), int(t[2]), int(t[3]), t[4])
+            tasks.append(task)
+        file.close()
 
         cap = cv2.VideoCapture('video1.mp4')
 
-        if 9 <= current_time < 14:
-            cap = cv2.VideoCapture("video2.mp4")
-        if 14 <= current_time < 17:
-            cap = cv2.VideoCapture("video3.mp4")
-        if 17 <= current_time < 23:
-            cap = cv2.VideoCapture("video4.mp4")
+        for task in tasks:
+            if task.h_start * 60 + task.m_start <= current_h * 60 + current_m < task.h_stop * 60 + task.m_stop:
+                cap = cv2.VideoCapture(task.path)
+                break
 
         if not cap.isOpened():
             print("Error opening video  file")
